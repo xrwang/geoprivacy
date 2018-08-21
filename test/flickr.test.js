32176ffd08285152c5f1c58c43be473a17b8e45c
xrwang <xiaoweirwang@gmail.com>
2018-08-21 13:02:34 -0700

    get tags for photos

--- test/flickr.test.js
+++ test/flickr.test.js
@@ -2,6 +2,8 @@ const test = require('tape')
 const flickr = require('../flickr.js')
 const testLocationResults = require('../test/arrayOfExifData');
 const expectedExifLocationList = require('../test/expectedEXIFLocationOnly');
+const photoListWithGPSCamera = require('../test/photoListWithGPSCamera');
+const photoListWithGPSCameraTags = require('../test/photoListWithGPSCameraTags');
 
 test('an input long list of photos gives only photos that have GPS exif data', function (t) {
   const result = flickr.filterArrayForExifOnly(testLocationResults)
@@ -9,4 +11,13 @@ test('an input long list of photos gives only photos that have GPS exif data', f
   t.ok(result)
   t.deepEqual(result, expected)
   t.end()
+});
+
+test('an input list of photos with GPS and Camera data gets tags appended to it', function (t) {
+  const result = flickr.getTagsForEachGeoPhoto(photoListWithGPSCamera).then((result) => {
+    const expected = photoListWithGPSCameraTags;
+    t.ok(result);
+    t.deepEqual(result, expected);
+    t.end();
+  });
 });
